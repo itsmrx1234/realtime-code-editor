f0bd1fcf1f505fc89a87f34e5f569c5938d0f1b7
Vaibhav <2k22.cscys.2211080@gmail.com>
2024-06-17 17:00:37 +0530

    fix bugs

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const http = require('http');
 const path = require('path');
 const { Server } = require('socket.io');
 const ACTIONS = require('./src/Actions');
+require('dotenv').config();
+
 
 const server = http.createServer(app);
 const io = new Server(server);
